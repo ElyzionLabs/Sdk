51f429dcb9deaa802d9dcef4d0e7da0b96430ac1
Elyzion <gadrfhh@gmail.com>
2026-04-06 22:35:03 +0700

    Create language_module.py

---
+++ language/language_module.py
@@ -0,0 +1,10 @@
+class LanguageModule:
+    def process(self, task, visual_data):
+        print("[Language] Processing task...")
+
+        # Simulasi interpretasi
+        return {
+            "action": "click",
+            "target": "button",
+            "confidence": 0.95
+        }
